35be41adb22043d5cfb66aca138ae8ea2515718c
Martín Rivera <A01749620@tec.mx>
2025-05-11 18:49:40 -0700

    Deploy try 5

--- antitesis/next.config.ts
+++ antitesis/next.config.ts
@@ -1,19 +1,8 @@
-import type { NextConfig } from "next";
-
-// Configuración tipada para Next.js
-const nextConfig: NextConfig = {
-  // Configuración para exportación estática
+const nextConfig = {
   output: 'export',
-  
-  // Optimización de imágenes (necesario para exportaciones estáticas)
   images: {
     unoptimized: true,
   },
-  
-  // Opciones de configuración adicionales
-  reactStrictMode: true,
-  
-  
 };
 
 export default nextConfig;
